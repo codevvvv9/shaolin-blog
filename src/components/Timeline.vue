8baaf27b92cd291ecb51ef53c449b0bc4fc71ec7
wushaolin <15265249516@163.com>
2020-11-21 19:24:44 +0800

    What: 完成tab页切换以及博客post抽象组件，单元测试通过
    Why:
    
    *
    
    How:
    
    *

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,23 +1,68 @@
 <template>
   <nav class="is-primary panel">
     <p class="panel-tabs">
-      <a data-test="period" v-for="(period, index) in periods" :key="index">
+      <a
+        :class="[period === selectedPeriod ? 'is-active' : '']"
+        data-test="period"
+        v-for="(period, index) in periods"
+        :key="index"
+        @click="setPeriod(period)"
+      >
         {{ period }}
       </a>
     </p>
+
+    <TimelinePost v-for="post in posts" :key="post.id" :post="post"/>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, computed } from "vue";
 import { Period } from "@/types";
+import { todayPost, thisWeek, thisMonth } from "@/mock";
+import moment from "moment";
+import TimelinePost from "./TimelinePost.vue";
 export default defineComponent({
   name: "Timeline",
+  components: {
+    TimelinePost
+  },
   setup() {
     const periods: Period[] = ["今天", "本周", "本月"];
+    const selectedPeriod = ref<Period>("今天");
+    const setPeriod = (period: Period) => {
+      selectedPeriod.value = period;
+    };
 
+    const posts = computed(() =>
+      [todayPost, thisWeek, thisMonth].filter((post) => {
+        if (
+          selectedPeriod.value === "今天" &&
+          post.created.isAfter(moment().subtract(1, "day"))
+        ) {
+          return true;
+        }
+        if (
+          selectedPeriod.value === "本周" &&
+          post.created.isAfter(moment().subtract(7, "day"))
+        ) {
+          return true;
+        }
+        if (
+          selectedPeriod.value === "本月" &&
+          post.created.isAfter(moment().subtract(1, "month"))
+        ) {
+          return true;
+        }
+
+        return false;
+      })
+    );
     return {
       periods,
+      selectedPeriod,
+      setPeriod,
+      posts,
     };
   },
 });
--- tests/unit/Timeline.spec.ts
+++ tests/unit/Timeline.spec.ts
@@ -1,10 +1,31 @@
-import { mount } from '@vue/test-utils'
-import Timeline from '@/components/Timeline.vue'
+import { mount } from "@vue/test-utils";
+import Timeline from "@/components/Timeline.vue";
 
-describe('Timeline.vue', () => {
-  it('Timeline组件中会有3个a标签', () => {
-    const wrapper = mount(Timeline)
+describe("Timeline.vue", () => {
+  it("测试Timeline组件中会有3个a标签", () => {
+    const wrapper = mount(Timeline);
     // console.log("wrapper", wrapper.html())
-    expect(wrapper.findAll("[data-test='period']")).toHaveLength(3)
-  })
-})
+    expect(wrapper.findAll("[data-test='period']")).toHaveLength(3);
+  });
+  it("测试Timeline组件中a标签的点击事件功能", async () => {
+    const wrapper = mount(Timeline);
+    const $today = wrapper.findAll("[data-test='period']")[0];
+    const $thisWeek = wrapper.findAll("[data-test='period']")[1];
+    const $thisMonth = wrapper.findAll("[data-test='period']")[2];
+    expect($today.classes()).toContain("is-active");
+
+    await $thisWeek.trigger("click");
+    expect($today.classes()).not.toContain("is-active");
+    expect($thisWeek.classes()).toContain("is-active");
+
+    await $thisMonth.trigger("click");
+    expect($today.classes()).not.toContain("is-active");
+    expect($thisWeek.classes()).not.toContain("is-active");
+    expect($thisMonth.classes()).toContain("is-active");
+  });
+  it("测试Timeline组件中会有3个a标签", () => {
+    const wrapper = mount(Timeline);
+    // console.log("wrapper", wrapper.html())
+    expect(wrapper.findAll("[data-test='period']")).toHaveLength(3);
+  });
+});
